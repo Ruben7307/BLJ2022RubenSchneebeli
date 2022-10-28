5524926d83327bdb4308d75b0d41663abf540162
Rubenizz <112400838+Rubenizz@users.noreply.github.com>
2022-10-28 08:50:07 +0200

    added files to github

--- C/KW43/AnalogUhr/cmake-build-debug/cmake_install.cmake
+++ C/KW43/AnalogUhr/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Ruben/CLionProjects/AnalogUhr
+# Install script for directory: C:/Github/BLJ2022RuSch/C/KW43/AnalogUhr
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Ruben/CLionProjects/AnalogUhr/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Github/BLJ2022RuSch/C/KW43/AnalogUhr/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
