84f41db57948bcd51a0e7ad0560754f4547d6740
Rubenizz <112400838+Rubenizz@users.noreply.github.com>
2022-11-11 13:41:14 +0100

    added code to rucksackpacken

--- Java/KW45/rucksackpacken/src/rucksack.java
+++ Java/KW45/rucksackpacken/src/rucksack.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.io.IOException;
 
 public class rucksack {
     public static void main(String[] args) {
@@ -7,13 +8,14 @@ public class rucksack {
         Scanner myObj = new Scanner(System.in);
         String ANSI_PURPLE = "\u001B[35m";
         String ANSI_RESET = "\u001B[0m";
+        String ANSI_RED = "\u001B[31m";
 
         System.out.println(ANSI_PURPLE + "**********************************");
         System.out.println("|  welcome to the backpack game  |");
         System.out.println("**********************************" + ANSI_RESET);
 
  while (true) {
-     System.out.println("\nWhat do you want to put in your backpack?        enter 0 to quit");
+     System.out.println("\nWhat do you want to put in your backpack?" + ANSI_RED + "\tenter 0 to quit" + ANSI_RESET);
      String a = myObj.nextLine();
 
      if (a.equals("0")){
@@ -28,17 +30,23 @@ public class rucksack {
          }
 }
 
-     items.add(a);
 
-     System.out.println("I take withe me...");
+         items.add(a);
 
-     for (int i = 0; i < items.size(); i++) {
-         System.out.println("-" + items.get(i));
 
-     }
+         System.out.println("I take withe me...");
+
+         for (int i = 0; i < items.size(); i++) {
+             System.out.println("-" + items.get(i));
+
+         }
+
+
+
 
 
 
  }
     }
 }
+
