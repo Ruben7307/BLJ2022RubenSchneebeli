f3547adcf170193cd2e069c989c7aee449d44fe8
Rubenizz <ruben.schneebeli@edu.tbz.ch>
2022-12-02 15:12:56 +0100

    made geburtstag challange

---
+++ Java/KW48/GeburtstagChallenge/src/Person.java
@@ -0,0 +1,40 @@
+import java.time.LocalDate;
+public class Person {
+    private int year;
+
+    private int month;
+
+    private int age;
+    private int day;
+
+    private String name;
+
+
+    public Person(int year, int moth, int day, String name) {
+        this.year = year;
+        this.month = moth;
+        this.day = day;
+        this.name = name;
+        this.age = LocalDate.now().minusYears(year).minusMonths(month - 1).minusDays(day).getYear();
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
