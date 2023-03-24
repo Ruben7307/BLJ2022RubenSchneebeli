a771be496559234b02723f5f2c174bd891c49bb2
Rubenizz <ruben.schneebeli@edu.tbz.ch>
2023-03-24 08:21:21 +0100

    Ordner verschoben und gelöscht

---
+++ Java/KW12/GeometryChallenge/src/main/java/ch/noseryoung/main/TriangleManager.java
@@ -0,0 +1,93 @@
+package ch.noseryoung.main;
+
+import java.util.ArrayList;
+
+public class TriangleManager {
+
+  private ShapeSideCreator generator;
+  private TriangleValidator triangleValidator;
+  private ArrayList<Triangle> triangles;
+
+  public TriangleManager(ShapeSideCreator generator) {
+    this.generator = generator;
+    triangleValidator = new TriangleValidator();
+    triangles = new ArrayList<>();
+  }
+
+  /**
+   * This method is the entry point for the triangle manager.
+   * It creates at least one triangle with the <code>ShapeSideCreator</code> instance
+   * and prints the triangle with its information.
+   */
+  public void run() {
+
+    printWelcomeText();
+
+    //todo: create multiple triangles with its validation
+
+    Triangle triangle = generateTriangle();
+    boolean isEquilateral = triangleValidator.isEquilateral(triangle);
+    boolean isIsosceles = triangleValidator.isIsosceles(triangle);
+    boolean isScalene = triangleValidator.isScalene(triangle);
+
+    // todo: add a valid Triangle to the triangles list
+    printTriangleInfo(triangle, isEquilateral, isIsosceles, isScalene);
+  }
+
+  /**
+   * This method calls an implementation from the <code>ShapeSideCreator</code> Interface
+   * to get three sides for a new triangle.
+   *
+   * @return created triangle
+   */
+  private Triangle generateTriangle() {
+    return new Triangle(generator.createSide(), generator.createSide(), generator.createSide());
+  }
+
+  /**
+   * This Method prints out the Triangle Information with the result form the validation.
+   *
+   * @param t
+   * @param isEquilateral
+   * @param isIsoceles
+   * @param isScalene
+   */
+  private void printTriangleInfo(Triangle t, boolean isEquilateral, boolean isIsoceles, boolean isScalene) {
+    // todo
+  }
+
+  /**
+   * This method returns out all generated Triangles, which are sorted by side.
+   * The Triangles are compared with its shortest side. The longest "shortest Side" is listed first.
+   *
+   * @return sorted triangle list
+   */
+  public ArrayList<Triangle> getAllTrianglesSortedByShortestSide(){
+    // todo
+    return null;
+  }
+
+  /**
+   * This method returns out all generated Triangles, which are sorted by side.
+   * The Triangles are compared with its longest side. The longest "longest Side" is listed first.
+   *
+   * @return sorted triangle list
+   */
+  public ArrayList<Triangle> getAllTrianglesSortedByLongestSide(){
+    // todo
+    return null;
+  }
+
+  /**
+   * This method prints a welcome text for the triangle manager.
+   */
+  private void printWelcomeText(){
+    // todo
+    System.out.println(" __      __       .__                                  __                            __         .__                     .__                                                             \n" +
+            "/  \\    /  \\ ____ |  |   ____  ____   _____   ____   _/  |_ ____     _____ ___.__. _/  |________|_______    ____   ____ |  |   ____     _____ _____    ____ _____    ____   ___________ \n" +
+            "\\   \\/\\/   _/ __ \\|  | _/ ___\\/  _ \\ /     \\_/ __ \\  \\   __/  _ \\   /     <   |  | \\   __\\_  __ |  \\__  \\  /    \\ / ___\\|  | _/ __ \\   /     \\\\__  \\  /    \\\\__  \\  / ___\\_/ __ \\_  __ \\\n" +
+            " \\        /\\  ___/|  |_\\  \\__(  <_> |  Y Y  \\  ___/   |  |(  <_> ) |  Y Y  \\___  |  |  |  |  | \\|  |/ __ \\|   |  / /_/  |  |_\\  ___/  |  Y Y  \\/ __ \\|   |  \\/ __ \\/ /_/  \\  ___/|  | \\/\n" +
+            "  \\__/\\  /  \\___  |____/\\___  \\____/|__|_|  /\\___  >  |__| \\____/  |__|_|  / ____|  |__|  |__|  |__(____  |___|  \\___  /|____/\\___  > |__|_|  (____  |___|  (____  \\___  / \\___  |__|   \n" +
+            "       \\/       \\/          \\/            \\/     \\/                      \\/\\/                           \\/     \\/_____/           \\/        \\/     \\/     \\/     \\/_____/      \\/       ");
+  }
+}
